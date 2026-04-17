aef9992655a26ac5c5a157c1f92ab679ca61de50
alexandrastoicagit <alexa9808@yahoo.com>
2026-04-18 00:35:51 +0300

    new18

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ setInterval(() => {
 
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
 
   document.getElementById("countdown").innerText =
     days + " zile • " + hours + " ore"+ minutes + " minute"+ seconds + " secunde";
